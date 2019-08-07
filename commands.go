7ce6f1d07b422d67948b6f193ad9f0d2a726f1e1
Patchkat <slingertv22@gmail.com>
2019-08-07 16:59:53 -0400

    GAPBOT-2 (#12)
    * Help updated
    
    Now appears in the help command
    
    * you saw nothing
    
    * updooted
    
    New commands
    
    * removed commands that don't exist here
    
    * Help updated
    
    Now appears in the help command
    
    * you saw nothing
    
    * updooted
    
    New commands
    
    * removed commands that don't exist here
    
    * Purge command on the way
    
    * Help updated
    
    Now appears in the help command
    
    * you saw nothing
    
    * updooted
    
    New commands
    
    * removed commands that don't exist here
    
    * Purge command on the way
    
    * Help updated
    
    Now appears in the help command
    
    * you saw nothing
    
    * updooted
    
    New commands
    
    * removed commands that don't exist here
    
    * basic semi-purge like thing
    
    * see if it actually runs the code for purge
    
    * should match with master embed now
    
    * technically works
    
    * need to get the number from the user
    
    * idk but it just broken, I mean purge might work
    
    * I mean it's just being dumb at this point
    
    * I mean I think it works now
    
    * Almost done, need it to not delete user message
    
    * Done
    Two commands
    purge x amount of messages
    purge 50 messages by entering nothing
    
    * Updated purge 
    
    might be a bit more efficient
    now uses bulk delete after converting all messages to message ids
    
    * updated README
    
    now in line with other branches
    
    * correctly updated README
    
    * config shouldn't be tracked
    
    gonna need to fix that on my computer or the master gitignore
    
    * more consistent help message
    
    formatting consistent between multi word commands
    
    * added admin help embed
    
    * no more auto purge
    
    Just deletes the command
    also always deletes the command
    
    * consolidated logic for purge and fixed some indentation
    
    * updated purge tooltip

--- commands.go
+++ commands.go
@@ -7,21 +7,27 @@ import (
 	"fmt"
 	"strings"
 
+	"strconv"
+
 	"github.com/bwmarrin/discordgo"
 )
 
+//Help command
 func Help(s *discordgo.Session, m *discordgo.MessageCreate) {
 	s.ChannelMessageSendEmbed(m.ChannelID, getHelpEmbed())
 }
 
+//DefaultHelp command
 func DefaultHelp(s *discordgo.Session, m *discordgo.MessageCreate) {
 	s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("%s isn't a valid command. Use %shelp to learn more", strings.TrimPrefix(m.Content, configData.Prefix), configData.Prefix))
 }
 
+//Ping command
 func Ping(s *discordgo.Session, m *discordgo.MessageCreate) {
 	s.ChannelMessageSend(m.ChannelID, "Pong!")
 }
 
+//Avatar command
 func Avatar(s *discordgo.Session, m *discordgo.MessageCreate) {
 	if len(m.Mentions) > 0 {
 		if len(m.Mentions) > 4 {
@@ -34,4 +40,28 @@ func Avatar(s *discordgo.Session, m *discordgo.MessageCreate) {
 		return
 	}
 	s.ChannelMessageSendEmbed(m.ChannelID, getAvatarEmbed(m.Author))
+}
+
+//Purge command
+func Purge(s *discordgo.Session, m *discordgo.MessageCreate) {
+	fields := strings.Fields(m.Content)
+	n, err := strconv.Atoi(fields[len(fields)-1])
+	if err != nil {
+		s.ChannelMessageSend(m.ChannelID, "Make sure a number of messages to delete is specified at the end of the command")
+		return
+	}
+	if n > 100 || n < 1 {
+		s.ChannelMessageSend(m.ChannelID, "Please enter a number between 1 and 100")
+		return
+	}
+	var messageIDs []string
+	messages, err := s.ChannelMessages(m.ChannelID, n+1, "", "", "")
+	if err != nil {
+		fmt.Printf("Error getting messages: %s", err)
+		return
+	}
+	for _, element := range messages {
+		messageIDs = append(messageIDs, element.ID)
+	}
+	s.ChannelMessagesBulkDelete(m.ChannelID, messageIDs)
 }
