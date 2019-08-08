a6dee4a7b28dd291e75f588c95c8af6ebdaeb2c3
morzack <john_kesler@caryacademy.org>
2019-08-08 16:57:35 -0400

    reimplemented 99 message purge limit

--- embeds.go
+++ embeds.go
@@ -4,8 +4,6 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/bwmarrin/discordgo"
 )
 
@@ -51,7 +49,7 @@ func getAdminHelpEmbed() *discordgo.MessageEmbed {
 	embed.Fields = []*discordgo.MessageEmbedField{
 		&discordgo.MessageEmbedField{
 			Name:   "Admin",
-			Value:  "`purge #` - purge a given number of messages (up to 100) from a channel",
+			Value:  "`purge #` - purge a given number of messages (up to 99) from a channel",
 			Inline: false,
 		},
 	}
